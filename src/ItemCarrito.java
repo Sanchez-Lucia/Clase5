92750f0a14955ec347b1f8d470331500da271153
Lucía Sánchez <116378686+Sanchez-Lucia@users.noreply.github.com>
2023-03-14 14:56:54 -0300

    Modificacion

--- src/ItemCarrito.java
+++ src/ItemCarrito.java
@@ -4,4 +4,9 @@ public class ItemCarrito {
     private Producto producto = new Producto();
     private Float precioUnitario;
 
+    public Float calcularPrecioTotal() {
+        float precio=0;
+        precio= this.precioUnitario * this.producto.costoFinal(1);
+        return precio;
+    }
 }
--- src/Producto.java
+++ src/Producto.java
@@ -4,8 +4,21 @@ public class Producto {
     private String nombre;
     private Float precio;
 
+    private static float porcentajeEnTresCuotas = 0.10f;
+
     public Float costoFinal(Integer cantCuotas) {
-        Float costo=0;
+        float costo=0;
+        switch (cantCuotas) {
+            case 1: {
+                costo=this.precio;
+                break;
+            }
+            case 3: {
+                costo = this.precio + (this.precio*porcentajeEnTresCuotas);
+                break;
+            }
+        }
+        return costo;
     }
 
 }
